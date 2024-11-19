a3b3d67c90d421a5a3a4afbf77135353fb7b8f1a
Saikaku <grnmnsta@gmail.com>
2024-11-19 10:47:33 +0900

    first commit

---
+++ PersonalityTest/PersonalityTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  PersonalityTestApp.swift
+//  PersonalityTest
+//
+//  Created by cmStudent on 2024/11/19.
+//
+
+import SwiftUI
+
+@main
+struct PersonalityTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
